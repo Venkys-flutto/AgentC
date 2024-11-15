9859a449b688e27ae77b83691bc5be7bb8da4eff
root <root@srv643024.hstgr.cloud>
2024-11-15 02:32:19 +0000

    Corrected URL

--- prod-webhook-handler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/AgentC
-git fetch origin
-git checkout prod
-git pull origin prod
-docker-compose down
-docker-compose up -d --build
-echo "Production environment updated and restarted."
